a1e3037d9e55e9cb79fb54d30ce1fca9e4b0f05e
sourav190495 <sourav19045@gmail.com>
2020-02-24 13:04:53 +1100

    final commit

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
  
 class TodoList extends Component {
   constructor(props){
   super(props);
+
   this.state = {
    items: []
 };
   this.addItem = this.addItem.bind(this);
-   }
+  this.deleteItem = this.deleteItem.bind(this);
+}
+
+deleteItem(key) {
+  var filteredItems = this.state.items.filter(function (item) {
+    return (item.key !== key);
+  });
+ 
+  this.setState({
+    items: filteredItems
+  });
+}
+
+
+
+
 addItem(e){
 if(this._inputElement.value !==""){
 var newItem = {
   text: this._inputElement.value,
   key: Date.now()
 };
+
+
   this.setState((prevState) => {
 return {
 items: prevState.items.concat(newItem)
@@ -40,7 +60,9 @@ e.preventDefault();
             </input>
             <button type="submit">add</button>
           </form>
-        </div>
+        </div>      
+        <TodoItems entries={this.state.items}
+                      delete={this.deleteItem}/>
       </div>
     );
   }
